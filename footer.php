5bcf600482dd39ed7271b31deaf826d9cc205196
terenceho <terenceho.82@gmail.com>
2015-04-15 19:58:29 +1000

    Test001
    test

---
+++ footer.php
@@ -0,0 +1,25 @@
+	<div class="footer">
+		<div class="footerLeftContainer">
+			<div class="footerLeft1">
+				<a href="#">ABOUT&nbsp;US</a> | 
+				<a href="#">CAREER</a> | 
+				<a href="#">FAQ</a> | 
+				<a href="#">PRIVACY&nbsp;POLICY</a> | 
+				<a href="#">TERMS&nbsp;&&nbsp;CONDITIONS</a>
+			</div>
+			<div class="footerLeft2">&copy; Copyright 1998-2015 Silverado Cinema Limited. All rights reserved.</div>
+		</div>
+
+
+		<div class="footerRightContainer">
+				<div class="footerRight">Student ID: 3382263 | Jeannine Tan</div>
+				<div class="footerRight">Student ID: 3452433 | Chun Man HO</div>
+				<div class="footerRight">
+				     &copy; Web Programming Assignment 2 |
+				     <a href="http://validator.w3.org/check?uri=referer"><img style="border:0;height:31px"
+				        src="http://www.w3.org/html/logo/downloads/HTML5_Logo_32.png" alt="HTML Validator" /></a> |
+				     <a href="http://jigsaw.w3.org/css-validator/check/referer"><img style="border:0; height:31px"
+				        src="http://jigsaw.w3.org/css-validator/images/vcss-blue" alt="CSS Validator" /></a>
+			     </div>
+		</div>
+	</div>
